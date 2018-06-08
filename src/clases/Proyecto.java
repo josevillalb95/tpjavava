82508c9270eebed99e127136b97fbda97766fb36
TomasGimenezR <tomasgimenezr@gmail.com>
2018-06-08 01:15:32 -0300

    Javadoc de Proyecto
    Se realiza el Javadoc de la gran mayoria del Javadoc de Proyecto

--- src/clases/Proyecto.java
+++ src/clases/Proyecto.java
@@ -34,6 +34,10 @@ public final class Proyecto {
 	
 	private static Proyecto instance = null;
 	
+	/**
+	 * Retorna la unica instancia de Proyecto existente
+	 * @return un Proyecto
+	 */
 	public static Proyecto getInstance(){
 		
 		if (instance == null)
@@ -42,11 +46,18 @@ public final class Proyecto {
 		return instance; 	
 	}
 	
+	/**
+	 * Retorna un Backlog
+	 * @return Backlog
+	 */
 	public Backlog getBlog() {
 		return blog;
 	}
-
-	public void diasig(){
+	
+	/**
+	 * Adelanta un dia para procesar los cambios de los Sprints y Tareas
+	 */
+	public void diaSig(){
 		today= today.plus(1, ChronoUnit.DAYS);
 		for(Sprint c:LSprints){
 			if(c.getFechaFin().compareTo(today)==1);{
@@ -61,6 +72,11 @@ public final class Proyecto {
 		
 	}
 	
+	/**
+	 * Genera una alta de un nuevo Sprint
+	 * @param clave Se le asignara la clave al nuevo Sprint
+	 * @param descripcion Se le asignara la descripcion al nuevo Sprint
+	 */
 	public void altaSprint(String clave , String descripcion) {
 		LSprints.add(new Sprint(clave, descripcion));
 	}
@@ -73,6 +89,11 @@ public final class Proyecto {
 		return LSprints;
 	}
 	
+	/**
+	 * Da de baja un Sprint de la lista con la clave indicada
+	 * @param clave Clave del Sprint a buscar para dar de baja
+	 * @throws SprintNoValido cuando el Sprint es ENCURSO o FINALIZADO, los cuales no pueden ser dados de baja
+	 */
 	public void bajaSprint(String clave) throws SprintNoValido{
 		boolean bandera = true;
 		Iterator<Sprint> it = LSprints.iterator();
@@ -95,6 +116,11 @@ public final class Proyecto {
 		}
 	}
 	
+	/**
+	 * Permite actualizar el Sprint pasado como parametro
+	 * @param clave Clave del Sprint a modificar
+	 * @param descricion Descripcion que se desea actualizar
+	 */
 	public void modificacionSprint(String clave ,String descricion){
 		for(Sprint c:LSprints){
 			if(c.getClave().equals(clave)){
@@ -118,22 +144,28 @@ public final class Proyecto {
 		LSprints.
 	}*/
 	
+	/**
+	 * Agrega una Tarea a un String
+	 * @param idS Sprint al cual se le agregara una Tarea
+	 * @param idT Tarea que sera agregada al Sprint
+	 */
 	public void agregarTareasSprint(String idS,String idT){
-		Iterator<Sprint>it=LSprints.iterator();
+		Iterator<Sprint>its=LSprints.iterator();
+		Iterator<Tarea>itt;
 		Sprint sp=null;
+		Tarea tar=null;
 		boolean bandera=true;
-		while(it.hasNext() && bandera){
-			sp=it.next();
+		while(its.hasNext() && bandera){
+			sp=its.next();
 			if(sp.getClave().equals(idS)){
 				//sp.aSprintTarea(t);
 				bandera=false;
 			}
 		}
-		Iterator<Tarea>it1=blog.getLTareasP().iterator();
-		Tarea tar=null;
+		itt=blog.getLTareasP().iterator();
 		bandera=true;
-		while(it1.hasNext() && bandera){
-			tar=it1.next();
+		while(itt.hasNext() && bandera){
+			tar=itt.next();
 			if(tar.getId().equals(idT)){
 				sp.aSprintTarea(tar);
 				//blog.bajaTarea(tar.getId());
@@ -142,24 +174,53 @@ public final class Proyecto {
 			
 		}
 	}
+	
+	/**
+	 * Carga las tareas de un txt
+	 */
 	public void cargarTareas(){
 		blog.cargaListaTareas();
 	}
+	
+	/**
+	 * Imprime por pantalla la lista de tareas
+	 */
 	public void mostrarTareas(){
 		blog.muestraTareas();
 	}
 	
+	/**
+	 * Agrega una Tarea como dependencia de otra Tarea.
+	 * @param idT Id de Tarea a la que agregarle una dependencia.
+	 * @param idDep Id de la Tarea que sera agregada como dependencia.
+	 */
 	public void agregarDependencias(String idT,String idDep){
 		blog.agregaDependencia(idT, idDep);
 	}
+	
+	/**
+	 * Elimina una Dependencia de una Tarea.
+	 * @param idT Id de la Tarea a la que eliminarle la Dependencia.
+	 * @param idDep Id de la Dependencia a ser eliminada de la Tarea.
+	 */
 	public void eliminarDependencia(String idT,String idDep){
 		blog.bajaDependencia(idT, idDep);
 	}
 	
+	/**
+	 * Agrega un FlujoPaso a una Tarea.
+	 * @param idT Id de la Tarea a la que agregarle un FlujoPaso.
+	 * @param descripcion del FlujoPaso.
+	 * @param pasos del FlujoPaso.
+	 */
 	public void agregarFlujoPaso(String idT, String descripcion, int pasos){
 		blog.agregaFP(idT, descripcion, pasos);
 	}
 	
+	/**
+	 * Retorna todos los Sprints que se encuentren ENCURSO
+	 * @return Springs en Curso
+	 */
 	public TreeSet<Tarea> getTareasSprintEnCurso() {
 		Sprint sp = null;
 		boolean bandera = true;
@@ -173,6 +234,12 @@ public final class Proyecto {
 		else
 			return sp.getListaT();
 	}
+	
+	/**
+	 * Calcula la estimacion del Sprint indicado
+	 * @param idSprint Id del Sprint
+	 * @return la estimacion del Sprint
+	 */
 	public int calcularEstimacionSprint(String idSprint){
 		Sprint sp=null;
 		boolean bandera=true;
@@ -188,6 +255,11 @@ public final class Proyecto {
 		return estimacion;
 	}
 	
+	/**
+	 * Calcula la estimacion de la Historia indicada
+	 * @param idSprint Id del Sprint que contiene la Historia
+	 * @return la estimacion de la Historia
+	 */
 	public int calcularEstimacionHistoriaSprint(String idSprint){
 		Sprint sp=null;
 		boolean bandera=true;
@@ -203,6 +275,11 @@ public final class Proyecto {
 		return estimacion;
 	}
 	
+	/**
+	 * Da de baja una Tarea de un Sprint.
+	 * @param idSprint Id del Sprint al que se le dara de baja la Tarea.
+	 * @param idTarea Id de la Tarea que sera dada de baja.
+	 */
 	public void bajaTareaSprint(String idSprint,String idTarea){
 		Iterator<Sprint>it=LSprints.iterator();
 		Sprint sp=null;
@@ -217,20 +294,36 @@ public final class Proyecto {
 		}
 	}
 	
+	/**
+	 * Da de baja una tarea del Backlog.
+	 * @param idTarea Id de la Tarea a dar de baja del Backlog.
+	 */
 	public void bajaTareaBackLog(String idTarea){
 		blog.bajaTarea(idTarea);
 	}
 	/**
-	 * 
-	 * @param idT tarea a la que hay que agregarle la subTarea
-	 * @param idSubT subTarea que hay que agregar
+	 * Agregar Tarea como Subtarea de otra Tarea.
+	 * @param idT Id Tarea a la que se le agregara la Subtarea.
+	 * @param idSubT Id Tarea a agregar como Subtarea.
 	 */
 	public void agregaSubT(String idT,String idSubT){
 		blog.agregaSubTarea(idT, idSubT);
 	}
+	
+	/**
+	 * Eliminar Subtarea de una Tarea.
+	 * @param idT Id Tarea a la que se le eliminara la Subtarea.
+	 * @param idSubT Id Tarea a eliminar como Subtarea.
+	 */
 	public void eliminarSubT(String idT,String idSubT){
 		blog.bajaSubTarea(idT, idSubT);
 	}
+	
+	/**
+	 * Cambia el estado de un Sprint por el pasado como parametro.
+	 * @param idSprint Id del Sprint a cambiarle el estado.
+	 * @param estado Estado al que se le desea cambiar el Sprint.
+	 */
 	public void cambiarEstadoSprint(String idSprint,String estado){
 		
 		Iterator<Sprint>it=LSprints.iterator();
@@ -248,6 +341,11 @@ public final class Proyecto {
 			System.out.println("Ya hay un sprint en curso");
 	}
 	
+	/**
+	 * Busca un Sprint y lo retorna.
+	 * @param idSprint Id del Sprint a buscar
+	 * @return Sprint buscado.
+	 */
 	public Sprint devuelveSprint(String idSprint){
 		Iterator<Sprint>it=LSprints.iterator();
 		Sprint sp=null;
@@ -260,17 +358,31 @@ public final class Proyecto {
 		}
 		return sp;
 	}
+	
+	/**
+	 * Pone en curso un Sprint, asignandole Fecha de inicio y de fin.
+	 * @param idSprint Id del Sprint a poner en curso.
+	 * @param fi Fecha de inicio a asignar.
+	 * @param ff Fecha de fin a asignar.
+	 */
 	public void sprintEnCurso(String idSprint, LocalDate fi,LocalDate ff){
 		Sprint s=devuelveSprint(idSprint);
 		s.comenzar(fi, ff);
 	}
 	
+	
+	/**
+	 * Genera un avance en dias al Sprint indicado.
+	 * @param idSprint Sprint en el cual se generara el avance.
+	 */
 	public void avance(String idSprint){
 		Sprint s=devuelveSprint(idSprint);
+		LocalDate fecha;
+		int avance;
 		//LocalDate fecha=s.getfInicio().plusDays(1);
 		if(s.getEstado().toString().equals("ENCURSO")){
-			LocalDate fecha=s.getfAvance().plusDays(1);
-			int avance=(int)s.getfInicio().until(fecha, ChronoUnit.DAYS);
+			fecha = s.getfAvance().plusDays(1);
+			avance = (int)s.getfInicio().until(fecha, ChronoUnit.DAYS);
 			s.setAvance();
 			s.setfAvance(fecha);
 			System.out.println(s.getfAvance());
@@ -284,6 +396,7 @@ public final class Proyecto {
 			System.out.println("El sprint ya esta finalizado");
 	}
 	
+
 	public int cantAvance(String id){
 		Sprint s=devuelveSprint(id);
 		return s.getAvance();
